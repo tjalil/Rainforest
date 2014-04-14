bfdf222d8c4cb73f6acf434ab3fbd6eafe86f7a2
Taha Jalil <taha.jalil@gmail.com>
2014-04-14 18:58:44 -0400

    completed products_controller and corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rainforest::Application.routes.draw do
+
+  resources :products
+  root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
