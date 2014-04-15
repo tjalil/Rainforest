bbc9d87445a0d669072188535cad58b01fd8a782
Taha Jalil <taha.jalil@gmail.com>
2014-04-15 14:11:48 -0400

    added callback to capitalize name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: { only_integer: true }
+  before_save :capitalize_name
 
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("%0.2f", price_in_dollars)
+  end
+
+  private
+
+  def capitalize_name
+    self.name = self.name.capitalize
   end
 end
