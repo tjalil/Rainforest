2f36c8c64c4cdbd1e5420b41628ca585d3b88354
Taha Jalil <taha.jalil@gmail.com>
2014-04-15 17:41:13 -0400

    added bcrypt, created user model and new user view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rainforest::Application.routes.draw do
   resources :products
   root 'products#index'
 
+  resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414212542) do
+ActiveRecord::Schema.define(version: 20140415181919) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
