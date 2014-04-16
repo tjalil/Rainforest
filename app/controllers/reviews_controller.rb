8cba5edca54eb52b0225be2cb546e1c2987995a6
Taha Jalil <taha.jalil@gmail.com>
2014-04-16 14:24:13 -0400

    filled reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,21 +3,32 @@ class ReviewsController < ApplicationController
   before_action :load_product
 
   def show
+    @review = Review.new(params[:id])
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = @product.reviews.build(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to product_review_path, notice: "Successfully added your comment"
+    else
+      render :new
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.delete
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:comment, :user_id, :product_id)
+    params.require(:review).permit(:comment, :product_id)
   end
 
   def load_product
