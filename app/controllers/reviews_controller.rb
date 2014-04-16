2c361d5775a08ee3222c29312d1a3fda969a9220
Taha Jalil <taha.jalil@gmail.com>
2014-04-16 14:11:37 -0400

    migrated review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+  before_action :load_product
+
   def show
   end
 
@@ -10,6 +12,16 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :user_id, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
   end
 
 end
