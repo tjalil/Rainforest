f952ac93693aedf549dd89c23e8c61da84962e5e
Taha Jalil <taha.jalil@gmail.com>
2014-04-17 13:22:17 -0400

    able to show list of categories on product creation form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417162341) do
+ActiveRecord::Schema.define(version: 20140417164915) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140417162341) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "url"
+    t.integer  "category_id"
   end
 
   create_table "reviews", force: true do |t|
