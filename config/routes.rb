169d3d881fcab4c4edb0959ad20c5f4524f2bda8
Taha Jalil <taha.jalil@gmail.com>
2014-04-17 16:08:56 -0400

    added categories controller to show products belonging to categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rainforest::Application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  resources :categories, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
