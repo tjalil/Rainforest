3d3996a51773b4a7d38249ef3eaaf4ad8707f5d4
Taha Jalil <taha.jalil@gmail.com>
2014-04-17 12:27:12 -0400

    added category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
 
   has_many :reviews
   belongs_to :user
+  belongs_to :category
 
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: { only_integer: true }
